9b2abffa7c037e4ae9a4f193da11ea45fc31b103
Guilherme183008 <83517965+Guilherme183008@users.noreply.github.com>
2021-06-10 13:02:29 -0300

    Add files via upload

---
+++ LógicoDesWeb.sql
@@ -0,0 +1,28 @@
+/* Lógico_desWeb: */
+
+CREATE TABLE Paciente (
+    nome VARCHAR(220),
+    telefone VARCHAR(30),
+    cpf VARCHAR(30),
+    endereço VARCHAR(220),
+    cep VARCHAR(30),
+    email VARCHAR(30),
+    idpaciente INTEGER PRIMARY KEY
+);
+
+CREATE TABLE events (
+    title VARCHAR(220),
+    color VARCHAR(10),
+    start DATE,
+    end DATE,
+    fk_idpaciente INTEGER,
+    id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE Usuario (
+    idusuario INTEGER PRIMARY KEY,
+    nome VARCHAR(220),
+    nivel VARCHAR(30),
+    usuario VARCHAR(30),
+    senha VARCHAR(30)
+);
