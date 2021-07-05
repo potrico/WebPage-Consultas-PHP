bd57cc592989bdcceb84b643980aaafc0a32e5eb
potrico <83515720+potrico@users.noreply.github.com>
2021-07-05 20:10:44 -0300

    Delete projeto directory

--- projeto/model/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
-define('DBNAME', 'desweb');
-define('PORT', 3308);
-
-$conn = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME . ';', USER, PASS);
